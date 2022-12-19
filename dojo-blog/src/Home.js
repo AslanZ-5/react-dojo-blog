e64d2294706c92d4e9e206a959c695374413afed
мансур зурабов <asl.zurabov@gmail.com>
2022-12-19 17:19:10 +0300

    Handling Fetch Errors

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,15 +2,30 @@ import { useState, useEffect } from 'react';
 import ItemList from './ItemList';
 const Home = () => {
     const [blogs,setBlogs] = useState(null)
-    const [name,setName] = useState('musa')
+    const [isPending,setIsPending] = useState(true)
+    const [errFetch,setError] = useState(null)
   
    useEffect(() => {
     fetch('http://localhost:8000/blogs')
-        .then(data => data.json())
-        .then(data => setBlogs(data))
+        .then(data => {
+            if(!data.ok){
+                throw Error("Couldn't fetch the data")
+            }
+            return data.json()
+        } )
+        .then(data => {
+            setBlogs(data)
+            setIsPending(false)
+        })
+        .catch(err => {
+            setIsPending(false)
+            setError(err.message)
+        })
    },[])
     return ( 
         <div className="home">
+            { errFetch && <div>{errFetch}</div>}
+            { isPending && <div> Loading...</div>}
             { blogs && <ItemList  blogs={blogs} title="All blogs"/> }
        
             {/* <ItemList blogs={blogs.filter(item => item.author === 'mario')} title="All blogs"/> */}
