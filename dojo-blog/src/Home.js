fa4d5e0e1e9ca8215ad94f0f4d7c980bb8c83159
мансур зурабов <asl.zurabov@gmail.com>
2022-12-19 11:33:03 +0300

    state hook in ract

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,18 @@
+import { useState } from 'react';
+
 const Home = () => {
+    const [name,setName] = useState('aslan')
+    const changeName = () => {
+        if (name === 'aslan'){
+            setName('isa')
+        }else{
+            setName('aslan')
+        }
+    }
     return ( 
         <div className="home">
-            <h1>Welcome to Home Dear!!!!</h1>
+            <h1>Welcome to Home Dear {name}!!!!</h1>
+            <button onClick={changeName}>Change Name</button>
         </div>
      );
 }
