9f7684ecaa732bb434461a48526d2ddcb55f751f
мансур зурабов <asl.zurabov@gmail.com>
2022-12-19 17:47:57 +0300

    react using router

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,13 +1,20 @@
 import Navbar from './Navbar';
 import Home from './Home'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
   return (
+    <Router>
     <div className="App">
       <Navbar />
       <div className="content">
-        <Home />
+        <Switch>
+          <Route path='/home'>
+          <Home />
+          </Route>
+        </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
