1731725c0d0492381957cc78b030beb0391e2b1d
мансур зурабов <asl.zurabov@gmail.com>
2022-12-20 11:29:21 +0300

    delete blog

--- dojo-blog/src/BlogDetails.js
+++ dojo-blog/src/BlogDetails.js
@@ -1,8 +1,18 @@
 import {useParams} from 'react-router-dom'
 import useFetch from './useFetch';
+import { useHistory } from 'react-router-dom';
+
 const BlogDetails = () => {
     const {id} = useParams()
+    const history = useHistory()
     const {data:blog,isPending,errFetch} = useFetch('http://localhost:8000/blogs/'+id)
+    const deleteHandle = () => {
+        fetch('http://localhost:8000/blogs/'+id,{
+            method:"DELETE"
+        }).then(
+            history.push('/')
+        )
+    }
     return ( 
         <div className="blog-details">
             {isPending && <div> Loading...</div>}
@@ -11,6 +21,7 @@ const BlogDetails = () => {
             <h2>{blog.title}</h2>
             <p>Writen by: {blog.author}</p>
             <div>{blog.body}</div>
+            <button onClick={deleteHandle}>Delete</button>
             </article>}
             
         </div>
